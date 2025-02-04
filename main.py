58151a9a3f8fee2efa1626ada1bea11eec9958c2
Pedro <pedrosilv1512@gmail.com>
2025-02-03 21:30:05 -0300

    [fix] alteracoes-finais

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import messagebox, filedialog
+from tkinter import messagebox, filedialog, simpledialog
 from auth import register_user, login_user
 from signature_test import sign_file, verify_signature
 from db_helpers import get_user_public_key
@@ -7,6 +7,14 @@ from db import init_db
 
 init_db()
 
+def center_window(window, width, height):
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+    
+    x = (screen_width/2) - (width/2)
+    y = (screen_height/2) - (height/2)
+    
+    window.geometry(f'{width}x{height}+{int(x)}+{int(y)}')
 
 def login():
     user = entry_user.get()
@@ -27,6 +35,9 @@ def open_main_window(user):
     login_window.destroy()
     main_window = tk.Tk()
     main_window.title("Sistema de Assinaturas RSA")
+
+    center_window(main_window, 345, 428)
+    main_window.resizable(False, False)
     
     def sign_document():
         file_path = filedialog.askopenfilename()
@@ -37,11 +48,10 @@ def open_main_window(user):
             else:
                 messagebox.showerror("Erro", message)
 
-
     def verify_document():
         file_path = filedialog.askopenfilename()
         if file_path:
-            username = tk.simpledialog.askstring("Verificação", "Nome do usuário que assinou:")
+            username = simpledialog.askstring("Verificação", "Nome do usuário que assinou:")
             public_key = get_user_public_key(username)
             if public_key:
                 success, message = verify_signature(file_path, username)
@@ -60,6 +70,9 @@ def open_main_window(user):
 
 login_window = tk.Tk()
 login_window.title("Login - Sistema RSA")
+
+center_window(login_window, 345, 428)
+login_window.resizable(False, False)
 
 tk.Label(login_window, text="Usuário:").pack()
 entry_user = tk.Entry(login_window)
